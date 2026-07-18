d765f3d7497adf36c620c7264113fd2b621a0c1a
Yujaerim <dbwofla11@soongsil.ac.kr>
2026-07-18 15:26:07 +0900

    feat[#4]: PlayerStat 부분 정의완료
    - 데이터는 정리 완료
    - 계산로직은 하나로 통합해서 만들어야함
    
    - 현재는 데이터 저장은 스킬 , 직업 , 스탯 이렇게 3개를 따로따로 저장을 함

--- Assets/Scripts/Runtimes/jobRuntimeData.cs
+++ Assets/Scripts/Runtimes/jobRuntimeData.cs
@@ -1,6 +1,10 @@
-public class JobInstance
+/// <summary>
+/// 플레이어가 선택한 직업의 런타임 상태를 관리한다.
+/// </summary>
+public class RuntimeJobData
 {
-    public JobSO job;
+    /// <summary>현재 선택된 직업</summary>
+    public JobSO CurrentJob;
 
-    public bool selected;
+    public bool selected; 
 }
---
+++ Assets/Scripts/Runtimes/skillRuntimeInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+public class SkillRuntimeInfo
+{
+    public SkillSO Profile;
+
+    public bool IsUnlocked;
+
+    public bool IsEnabled;
+
+    public int PurchaseCount;
+}
